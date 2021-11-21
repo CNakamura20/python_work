56e6e1bf50b581b92c7a28a82cefd61af3b24cf1
CNakamura20 <carolinesantosmorais80@gmail.com>
2021-11-21 15:19:33 -0300

    test github changes

--- Coursera Python/bruninho_teste.py
+++ Coursera Python/bruninho_teste.py
@@ -1,6 +1,8 @@
 def hello (name):
-	print( "olá familia, " + name)
+	print( "Olá família, " + name)
 
 name_list= ["Bruno", "Carol", "Silvane", "Adair", "Jamile"]
 for name in name_list:
 	hello(name)
+	
+#teste para o github
